8d69cad1b48dbca15f01351fe400e1d6cbd3c18a
xtagm <denis.rousseau@xtagm.com>
2025-06-11 09:44:01 +0200

    Dev batches

---
+++ devclose.cmd
@@ -0,0 +1,16 @@
+@echo off
+if exist devinit goto close
+echo Dev not initiated, cannot close...
+goto fin
+:close
+copy .\js\ccomp\tc.js .\js\tc.js
+copy .\js\ccomp\tr.js .\js\tr.js
+copy .\js\ccomp\tm.js .\js\tm.js
+copy .\js\ccomp\tv.js .\js\tv.js
+copy .\js\ccomp\cc.js .\js\cc.js
+copy .\js\wap\ccomp\tp.js .\js\wap\tp.js
+copy .\js\wap\ccomp\ts.js .\js\wap\ts.js
+copy .\js\wap\ccomp\cp.js .\js\wap\cp.js
+ren devinit devclose
+:fin
+pause
--- devinit.cmd
+++ devinit.cmd
@@ -1,12 +1,16 @@
 @echo off
-cd js
-copy tc.js .\ccomp
-copy tr.js .\ccomp
-copy tm.js .\ccomp
-copy tv.js .\ccomp
-copy cc.js .\ccomp
-cd wap
-copy tp.js .\ccomp
-copy ts.js .\ccomp
-copy cp.js .\ccomp
+if exist devclose goto init
+echo Dev already initiated...
+goto fin
+:init
+copy .\js\tc.js .\js\ccomp\tc.js
+copy .\js\tr.js .\js\ccomp\tr.js
+copy .\js\tm.js .\js\ccomp\tm.js
+copy .\js\tv.js .\js\ccomp\tv.js
+copy .\js\cc.js .\js\ccomp\cc.js
+copy .\js\wap\tp.js .\js\wap\ccomp\tp.js
+copy .\js\wap\ts.js .\js\wap\ccomp\ts.js
+copy .\js\wap\cp.js .\js\wap\ccomp\cp.js
+ren devclose devinit
+:fin
 pause

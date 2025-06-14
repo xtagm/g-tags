d464a7a03fdd040526c571088c7d91f2dc19f218
Denis Rousseau <denis.rousseau@non.se.com>
2025-06-14 07:25:54 +0200

    Secure devinit for first call

--- devinit.cmd
+++ devinit.cmd
@@ -4,14 +4,14 @@ echo Dev already initiated...
 goto end
 :init
 echo -- Copy parent folder in dev -------------------
-copy .\js\tc.js .\js\ccomp\tc.js
-copy .\js\tr.js .\js\ccomp\tr.js
-copy .\js\tm.js .\js\ccomp\tm.js
-copy .\js\tv.js .\js\ccomp\tv.js
-copy .\js\cc.js .\js\ccomp\cc.js
-copy .\js\wap\tp.js .\js\wap\ccomp\tp.js
-copy .\js\wap\ts.js .\js\wap\ccomp\ts.js
-copy .\js\wap\cp.js .\js\wap\ccomp\cp.js
+if exist .\js\tc.js copy .\js\tc.js .\js\ccomp\tc.js
+if exist .\js\tr.js copy .\js\tr.js .\js\ccomp\tr.js
+if exist .\js\tm.js copy .\js\tm.js .\js\ccomp\tm.js
+if exist .\js\tv.js copy .\js\tv.js .\js\ccomp\tv.js
+if exist .\js\cc.js copy .\js\cc.js .\js\ccomp\cc.js
+if exist .\js\wap\tp.js copy .\js\wap\tp.js .\js\wap\ccomp\tp.js
+if exist .\js\wap\ts.js copy .\js\wap\ts.js .\js\wap\ccomp\ts.js
+if exist .\js\wap\cp.js copy .\js\wap\cp.js .\js\wap\ccomp\cp.js
 ren devclose devinit
 :end
 pause
